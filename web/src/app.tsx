c7cbf9d854598c92bb46cebda8a6b4e29d7720f8
Rafael de Lima Thomaz <79thomaz@gmail.com>
2025-11-26 13:47:56 -0300

    frontend initialization

---
+++ web/src/app.tsx
@@ -0,0 +1,9 @@
+import { UploadWidget } from "./components/upload-widget";
+
+export function App() {
+  return (
+    <main className="h-dvh flex flex-col items-center">
+      <UploadWidget />
+    </main>
+  );
+}
